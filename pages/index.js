0229617357eec133f590e1a8ccc7ada87aff08e0
Rik Frieling <rik@rikBook.local>
2018-08-08 18:18:18 +0200

    Resolve merge conflicts

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
 import Header from '../components/Header/Header';
 
 export default () => (
   <div>
     <Header />
-=======
-
-export default () => (
-  <div>
-    Welcome to Team Awesome!
->>>>>>> master
   </div>
 );
