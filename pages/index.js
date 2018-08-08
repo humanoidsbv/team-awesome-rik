8d5b01caa702211cb0ad5dd21bf3595c12e4741e
Rik Frieling <rik@rikBook.local>
2018-08-08 18:15:31 +0200

    Merge master branch

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
+<<<<<<< HEAD
 import Header from '../components/Header/Header';
 
 export default () => (
   <div>
     <Header />
+=======
+
+export default () => (
+  <div>
+    Welcome to Team Awesome!
+>>>>>>> master
   </div>
 );
