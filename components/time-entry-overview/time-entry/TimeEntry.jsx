ebbd8fe30555cadd8f9113eb9dce952b49db0057
Rik Frieling <rik@rikBook.local>
2018-08-14 15:35:03 +0200

    fix overlooked review issues

--- components/time-entry-overview/time-entry/TimeEntry.jsx
+++ components/time-entry-overview/time-entry/TimeEntry.jsx
@@ -4,24 +4,21 @@ import PropTypes from 'prop-types';
 import './time-entry.scss';
 
 
-const TimeEntry = ({ employer, from, to }) => {
-  TimeEntry.propTypes = {
-    employer: PropTypes.string.isRequired,
-    from: PropTypes.string.isRequired,
-    to: PropTypes.string.isRequired
-  };
+const TimeEntry = ({ employer, from, to }) => (
+  <div className="time-entry">
+    <span>
+      {employer}
+    </span>
+    <span>
+      {`${from}-${to}`}
+    </span>
+  </div>
+);
 
-  return (
-    <div className="time-entry">
-      <span className="time-entry__item time-entry__item__employer">
-        {employer}
-      </span>
-      <span className="time-entry__item time-entry__item__time">
-        {`${from}-${to}`}
-      </span>
-    </div>
-  );
+TimeEntry.propTypes = {
+  employer: PropTypes.string.isRequired,
+  from: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired
 };
-
 
 export default TimeEntry;
