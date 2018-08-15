cbb43fc9bf0c30f245139e74005089dbbb81cd5b
Rik Frieling <rik@rikBook.local>
2018-08-15 12:45:13 +0200

    small fix to mapping of entries and change to data flow

--- components/time-entries/TimeEntries.jsx
+++ components/time-entries/TimeEntries.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
 import TimeEntry from '../time-entry/TimeEntry';
-import mockTimeEntry from './mockTimeEntry.json';
 import './time-entries.scss';
 
 
-const TimeEntries = () => (
+const TimeEntries = ({ mockTimeEntry }) => (
   mockTimeEntry.map((currentEntry, index, mockEntries) => (
     <React.Fragment key={currentEntry.id}>
       {(!index || currentEntry.date !== mockEntries[index - 1].date) && currentEntry.date}
-      <TimeEntry {... currentEntry} />
+      <TimeEntry {...currentEntry} />
     </React.Fragment>
   ))
 );
--- components/time-entry-overview/TimeEntryOverview.jsx
+++ components/time-entry-overview/TimeEntryOverview.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-
+import mockTimeEntry from './mockTimeEntry.json';
 import TimeEntryForm from '../time-entry-form/TimeEntryForm';
 import TimeEntries from '../time-entries/TimeEntries';
 import './time-entry-overview.scss';
 
 class TimeEntryOverview extends React.Component {
-  state = { isTimeEntryFormOpen: false };
+  state = { isTimeEntryFormOpen: false, mockTimeEntry };
 
   handleClick = () => {
     this.setState(({ isTimeEntryFormOpen }) => ({ isTimeEntryFormOpen: !isTimeEntryFormOpen }));
   };
+
+  // handleSubmit = (newEntry) => {
+  //   let updatedTimeEntry = []
+  //   this.setState(state => ({...state, mockTimeEntry: () }));
+  // };
+
 
   render() {
     const { isTimeEntryFormOpen } = this.state;
@@ -29,8 +35,8 @@ class TimeEntryOverview extends React.Component {
           />
           New time entry
         </button>
-        <TimeEntryForm />
-        <TimeEntries />
+        <TimeEntryForm handleSubmit={this.handleSubmit} />
+        <TimeEntries {...this.state} />
       </React.Fragment>
     );
   }
