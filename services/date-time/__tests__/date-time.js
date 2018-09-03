20081c9feeb936fee7928e742dc1507549fc5529
Rik Frieling <rik@rikBook.local>
2018-09-03 10:56:10 +0200

    update date-time test

--- services/date-time/__tests__/date-time.js
+++ services/date-time/__tests__/date-time.js
@@ -1,5 +1,5 @@
 import { createTimeStamp } from '../date-time';
 
 test('if timestamp is created using nl locale date and time', () => {
-  expect(createTimeStamp('01-01-1988', '09:00')).toBe('1988-01-01T08:00:00.000Z');
+  expect(createTimeStamp('01-01-1988', '09:00')).toBe('1988-01-01T09:00:00.000Z');
 });
