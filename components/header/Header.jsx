4f29259e2ea46c61a710cd93628a453a03f59df9
Rik Frieling <rik@rikBook.local>
2018-09-07 18:18:46 +0200

    update header css

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from 'next/link';
 
+
+import NavBar from './header-nav-bar/HeaderNavBar';
 import './header.scss';
 
 class Header extends React.Component {
@@ -12,10 +13,11 @@ class Header extends React.Component {
 
   handleClick = () => {
     this.props.toggleMenuOpen();
-  };
+  }
 
   render() {
-    const { isMenuOpen } = this.props;
+    const { isMenuOpen, toggleMenuOpen } = this.props;
+
     return (
       <header className="header">
         <div className="title-bar">
@@ -35,38 +37,12 @@ class Header extends React.Component {
             />
           </button>
         </div>
-        <nav className={`main-nav ${isMenuOpen ? 'main-nav--open' : ' main-nav'}`}>
-          <ul className="main-nav__nav-list">
-            <li className="main-nav__item main-nav__item--active">
-              <Link href="/time-entries">
-                <a>
-                  Timesheets
-                </a>
-              </Link>
-            </li>
-            <li className="main-nav__item">
-              <Link href="/team-members">
-                <a>
-                Team Members
-                </a>
-              </Link>
-            </li>
-            <li className="main-nav__item">
-              Projects
-            </li>
-            <li className="main-nav__item">
-              Clients
-            </li>
-            <li className="main-nav__item">
-              Documents
-            </li>
-          </ul>
-        </nav>
+        <NavBar isMenuOpen={isMenuOpen} toggleMenuOpen={toggleMenuOpen} />
         <button className="profile" type="button">
           <img className="logo" src="/static/logo.jpg" alt="logo" />
           <img className="picture" src="/static/rik.jpg" alt="pic" />
+          <img className="arrow-down" src="/static/icons/arrow-down.svg" alt="arrow" />
         </button>
-        <img className="arrow-down" src="/static/icons/arrow-down.svg" alt="arrow" />
       </header>
     );
   }
